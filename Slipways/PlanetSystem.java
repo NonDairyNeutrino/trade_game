5b1a341976e55bd92cce735115cb52803f037292
nate_chapman <chapmann19@gmail.com>
2022-08-07 21:57:31 -0700

    start overall planetary system

---
+++ Slipways/PlanetSystem.java
@@ -0,0 +1,19 @@
+/*
+Objective: Graph representation of the system of planets
+Author: Nathan Chapman
+Date: 8/7/2022
+*/
+
+import java.util.ArrayList;
+
+public class PlanetSystem {
+// CLASS VARIABLES
+  public static ArrayList<Planet> planets = new ArrayList<Planet>();
+
+// METHODS
+
+// MAIN
+  public static void main(String[] args) {
+    System.out.println("Test");
+  }
+}
