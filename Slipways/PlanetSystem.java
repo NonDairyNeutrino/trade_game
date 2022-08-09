91de362182a393bf058b208bf9b46542f55802b9
nate_chapman <chapmann19@gmail.com>
2022-08-09 00:18:28 -0700

    add untested extendGraph

--- Slipways/PlanetSystem.java
+++ Slipways/PlanetSystem.java
@@ -12,16 +12,27 @@ public class PlanetSystem {
   private static ArrayList<ArrayList<String> > graph;
 
 // METHODS
+  // create an empty planetary system
   public PlanetSystem () {
+    // initialize system with 0 planets
     planets = new ArrayList<Planet>();
-    graph   = new ArrayList<ArrayList<String> >(planets.size());
+    // initialize system with a null graph adjacency matrix
+    graph   = new ArrayList<ArrayList<String> >(0);
     for (int k = 0; k < graph.size(); k++) {
-      graph.set(k, new ArrayList<String>());
+      graph.set(k, new ArrayList<String>(0));
     }
+  }
+
+  private static void extendGraph () {
+    graph.forEach((row) -> row.add("")); // tack on another element at the end of each  row
+    graph.add(new ArrayList<String>(graph.size() + 1));    // add another row
+    graph.get(graph.size() - 1).set(graph.size() - 1, ""); // initialize the last element of the new row
   }
 
   public static void addPlanet (Planet p) {
     planets.add(p);
-    
+    extendGraph();
+
+    // TODO: add graph adjacency matrix
   }
 }
