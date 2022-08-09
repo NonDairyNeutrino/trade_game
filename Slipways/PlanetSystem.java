5fdfb184a30448882645beba7e1f4444149f7b7d
nate_chapman <chapmann19@gmail.com>
2022-08-09 00:22:00 -0700

    change "graph" to "network"

--- Slipways/PlanetSystem.java
+++ Slipways/PlanetSystem.java
@@ -9,30 +9,35 @@ import java.util.ArrayList;
 public class PlanetSystem {
 // CLASS VARIABLES
   public  static ArrayList<Planet> planets;
-  private static ArrayList<ArrayList<String> > graph;
+  private static ArrayList<ArrayList<String> > network;
 
 // METHODS
   // create an empty planetary system
   public PlanetSystem () {
     // initialize system with 0 planets
     planets = new ArrayList<Planet>();
-    // initialize system with a null graph adjacency matrix
-    graph   = new ArrayList<ArrayList<String> >(0);
-    for (int k = 0; k < graph.size(); k++) {
-      graph.set(k, new ArrayList<String>(0));
+    // initialize system with a null network adjacency matrix
+    network   = new ArrayList<ArrayList<String> >(0);
+    for (int k = 0; k < network.size(); k++) {
+      network.set(k, new ArrayList<String>(0));
     }
   }
 
   private static void extendGraph () {
-    graph.forEach((row) -> row.add("")); // tack on another element at the end of each  row
-    graph.add(new ArrayList<String>(graph.size() + 1));    // add another row
-    graph.get(graph.size() - 1).set(graph.size() - 1, ""); // initialize the last element of the new row
+    network.forEach((row) -> row.add("")); // tack on another element at the end of each  row
+    network.add(new ArrayList<String>(network.size() + 1));    // add another row
+    network.get(network.size() - 1).set(network.size() - 1, ""); // initialize the last element of the new row
+  }
+
+  private static void updateGraph (Planet p) {
+    extendGraph();
+
   }
 
   public static void addPlanet (Planet p) {
     planets.add(p);
     extendGraph();
 
-    // TODO: add graph adjacency matrix
+    // TODO: add network adjacency matrix
   }
 }
