0e83bee9246b710f068d57bc3fa7b12c9dfc8909
nate_chapman <chapmann19@gmail.com>
2022-08-26 16:29:33 -0700

    begin main gameplay loop

---
+++ Slipways/Slipways.java
@@ -0,0 +1,23 @@
+/*
+Objective: This program serves to execute the main gameplay loop of the text-based implementation of the Slipways game.
+Author: Nathan Chapman
+Date: 8/26/2022
+*/
+
+/*
+Outline
+-------
+1) Print initial prompt and introduction to the game ex. "Welcome to (text-based) Slipways where you can build your interplanetary trade empire!"
+
+2) Get user inputted actions e.g. discover, colonize, connect, trade.
+
+3) implement the action on the system via the backend
+
+4) return board state
+
+5) goto step 2 and loop!
+*/
+
+public class Slipways {
+
+}
