23fea13f749b6c1b175ee46e9db88547aceb1817
nate_chapman <chapmann19@gmail.com>
2022-08-26 16:46:11 -0700

    begin text prompts and main loop

--- Slipways/Slipways.java
+++ Slipways/Slipways.java
@@ -7,17 +7,34 @@ Date: 8/26/2022
 /*
 Outline
 -------
-1) Print initial prompt and introduction to the game ex. "Welcome to (text-based) Slipways where you can build your interplanetary trade empire!"
+1) TODO: Print initial prompt and introduction to the game ex. "Welcome to (text-based) Slipways where you can build your interplanetary trade empire!"
 
-2) Get user inputted actions e.g. discover, colonize, connect, trade.
+2) TODO: Generate random planets via a seed.  Seed can be given or random.
 
-3) implement the action on the system via the backend
+2) TODO: Get user inputted actions e.g. discover, colonize, connect, trade.
 
-4) return board state
+3) TODO: Implement the action on the system via the backend
+
+4) TODO: Return board state
 
 5) goto step 2 and loop!
 */
 
 public class Slipways {
 
+  private static String initPrompt = "Welcome to (text-based) Slipways; where you can build your interplanetary trade empire!/nTo begin you can either specify a seed for planet generation, or you can use a randomly generated one.";
+
+  private static String helpText = "";
+
+  private static String controlsText = "";
+
+  private static String seedText = "Please enter a seed for planet generation.  If you want a random seed, simply press 'Enter'";
+
+  public static void main(String[] args) {
+    System.out.println(initPrompt); // Display initial welcome and instructions.
+
+    System.out.println(seedText); // Display seed prompt
+
+    // TODO: get seed and build planet system
+  }
 }
