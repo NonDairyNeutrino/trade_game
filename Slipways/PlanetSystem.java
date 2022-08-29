c5e2802843120527c5976c11ae0e898efdd04d6a
nate_chapman <chapmann19@gmail.com>
2022-08-28 21:51:51 -0700

    fix system initialization

--- Slipways/PlanetSystem.java
+++ Slipways/PlanetSystem.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 public class PlanetSystem {
 // CLASS VARIABLES
   public ArrayList<Planet> planets;
-  private ArrayList<ArrayList<String> > network;
+  public ArrayList<ArrayList<String> > network;
+  // values of strings can be: 'u' for undiscovered, "d" for discovered, 'c1/2/3' for colonized at levels 1,2, or 3
 
 // METHODS
   // create an empty planetary system
@@ -17,23 +18,28 @@ public class PlanetSystem {
     // initialize system with 0 planets
     planets = new ArrayList<Planet>(0);
     // initialize system with a null network adjacency matrix
-    network   = new ArrayList<ArrayList<String> >(0);
-    for (int k = 0; k < network.size(); k++) {
-      network.set(k, new ArrayList<String>());
-    }
+    network = new ArrayList<ArrayList<String> >(0);
+    network.add(new ArrayList<String>(0));
   }
 
   // add a new row and column to the network adjacency matrix
   private void extendNetwork () {
-    network.forEach((row) -> row.add("")); // tack on another element at the end of each  row
-    network.add(new ArrayList<String>(network.size() + 1));    // add another row
-    network.get(network.size() - 1).set(network.size() - 1, ""); // initialize the last element of the new row
+
+    if (network.get(0).isEmpty()) {
+      network.add(new ArrayList<String>(1));
+    }
+    else {
+      network.forEach((row) -> row.add("")); // tack on another element at the end of each  row
+      network.add(new ArrayList<String>(network.size() + 1));    // add another row
+      network.get(network.size() - 1).set(network.size() - 1, ""); // initialize the last element of the new row
+    }
   }
 
   // when a
   public void addPlanet (Planet p) {
     planets.add(p);
     extendNetwork();
+    network.
   }
 
   // TODO: add  planet connecting
@@ -46,6 +52,11 @@ public class PlanetSystem {
   public void trade(Planet p){
 
   }
+
+  // public static String toString () {
+  //   // TODO: print board
+  //   return "";
+  // }
 
   public static void main(String[] args) {
     PlanetSystem empire = new PlanetSystem();
