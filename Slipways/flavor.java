9be4a97dc6853a4662c9c6ac28cbce50d235650d
nate_chapman <chapmann19@gmail.com>
2022-08-31 13:47:12 -0700

    start flavor

---
+++ Slipways/flavor.java
@@ -0,0 +1,7 @@
+/*
+Objective: provide story and flavor to the game
+Author: Nathan Chapman
+Date: 8/31/2022
+*/
+
+// The year is 1971.  You have been sent out to space on a windowless tube with only a terminal to guide you.
