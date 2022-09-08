4aa871634d9725fba62892a927ec29708e83ecb0
nate_chapman <chapmann19@gmail.com>
2022-09-08 16:18:39 -0700

    fix some stuff?

--- PlanetSystem.java
+++ PlanetSystem.java
@@ -10,28 +10,28 @@ public class PlanetSystem {
 // CLASS VARIABLES
   public ArrayList<Planet> planets;
   public ArrayList<ArrayList<String> > network;
-  // values of strings can be: 'u' for undiscovered, "d" for discovered, 'c1/2/3' for colonized at levels 1,2, or 3
+  // values of strings can be: "d" for discovered, 'c1/2/3' for colonized at levels 1,2, or 3
 
 // METHODS
   // create an empty planetary system
   public PlanetSystem () {
     // initialize system with 0 planets
-    planets = new ArrayList<Planet>(0);
+    planets = new ArrayList<Planet>();
     // initialize system with a null network adjacency matrix
-    network = new ArrayList<ArrayList<String> >(0);
-    network.add(new ArrayList<String>(0));
+    network = new ArrayList<ArrayList<String> >();
+    network.add(new ArrayList<String>());
   }
 
   // add a new row and column to the network adjacency matrix
   private void extendNetwork () {
 
     if (network.get(0).isEmpty()) {
-      network.add(new ArrayList<String>(1));
+      network.get(0).add(new ArrayList<String>());
     }
     else {
       network.forEach((row) -> row.add("")); // tack on another element at the end of each  row
       network.add(new ArrayList<String>(network.size() + 1));    // add another row
-      network.get(network.size() - 1).set(network.size() - 1, ""); // initialize the last element of the new row
+      network.get(network.size() - 1).set(network.size() - 1, "new"); // initialize the last element of the new row
     }
   }
 
@@ -39,7 +39,7 @@ public class PlanetSystem {
   public void addPlanet (Planet p) {
     planets.add(p);
     extendNetwork();
-    //network.
+    //network.set(network.size() - 1, )
   }
 
   // TODO: add  planet connecting
@@ -53,13 +53,14 @@ public class PlanetSystem {
 
   }
 
-  // public static String toString () {
-  //   // TODO: print board
-  //   return "";
-  // }
+  // display planet system
+  public void show () {
+    System.out.println(network);
+  }
 
   public static void main(String[] args) {
-    PlanetSystem empire = new PlanetSystem();
-    System.out.println(empire.planets);
+    PlanetSystem galaxy = new PlanetSystem();
+    galaxy.addPlanet(new Planet(0));
+    galaxy.show();
   }
 }
