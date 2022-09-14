0aa2e345b8fda594a09f04fd5693155179181635
nate_chapman <chapmann19@gmail.com>
2022-09-14 13:57:01 -0700

    remove dependencies; change needs/gives to arrays; update constructor

--- Planet.java
+++ Planet.java
@@ -4,33 +4,25 @@ Author: Nathan Chapman
 Date: 9/8/22
 */
 
-// TODO: add set of available resources
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-
 public class Planet {
   // Instrinsic properties
-  public String name; // randomly generated hex number
-  public int id;
-  private String needsGives = "NG", needs = "N_", gives = "_G", satisfied = "__";
+  public String name;
+  private int id;
+  public String needsGivesState = "NG", needsState = "N_", givesState = "_G", satisfiedState = "__";
   // values of strings can be:
   // "NG" = has unmet needs, and has things to give (default state)
   // "N_" = has unmet needs, but can't give any more
   // "_G" = has needs met, but still has resources to give
   // "__" = has needs met and no more resources to give
-  public String state = needsGives;
-  private String[] resources  = {"food", "water", "air", "materials", "people", "entertainment"}; // TODO: add resources
+  public String state = needsGivesState;
+  private String[] resources  = {"food", "water", "air", "materials", "people", "entertainment"};
 
-  public ArrayList<String> needsList = new ArrayList<String>();
-  public ArrayList<String> givesList = new ArrayList<String>();
+  public String[] needs = new String[2];
+  public String[] gives = new String[2];
 
-  public Planet(int id) {
+  public Planet(int id, String[] needs, String[] gives) {
     this.id = id;
-  }
-
-  public void discover () {
+    this.name = Integer.toHexString(id);
     this.needs = needs;
     this.gives = gives;
   }
