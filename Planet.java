28774abd2a3bac14ac9332f5308d4d6045e4e8b0
nate_chapman <chapmann19@gmail.com>
2022-09-14 17:41:27 -0700

    adjust code formatting

--- Planet.java
+++ Planet.java
@@ -5,34 +5,35 @@ Date: 9/8/22
 */
 
 public class Planet {
+
+  public static String[] resources  = {"food", "water", "air", "materials", "people", "entertainment"};
+  public static String needsGivesState = "NG", needsState = "N_", givesState = "_G", satisfiedState = "__";
+
   // Instrinsic properties
   public String name;
   private int id;
-  public String needsGivesState = "NG", needsState = "N_", givesState = "_G", satisfiedState = "__";
   // values of strings can be:
   // "NG" = has unmet needs, and has things to give (default state)
   // "N_" = has unmet needs, but can't give any more
   // "_G" = has needs met, but still has resources to give
   // "__" = has needs met and no more resources to give
-  public String state = needsGivesState;
-  public static String[] resources  = {"food", "water", "air", "materials", "people", "entertainment"};
-
+  public String   state = needsGivesState;
   public String[] needs = new String[2];
   public String[] gives = new String[2];
 
   public Planet(int count, int num, String[] needs, String[] gives) {
-    this.id = count;
-    this.name = Integer.toHexString(num).toUpperCase();
+    this.id    = count;
+    this.name  = Integer.toHexString(num).toUpperCase();
     this.needs = needs;
     this.gives = gives;
   }
 
   public String toString () {
-    String nameString = "Name: " + name;
+    String nameString  = "Name: " + name;
     String stateString = "\nState: " + state;
     String needsString = "\nNeeds: " + needs[0] +", " + needs[1];
     String givesString = "\nGives: " + gives[0] + ", " + gives[1];
     String connectionsString = "\nConnected to: " + "CONNCETIONS";
-    return  nameString + stateString + needsString + givesString + connectionsString + "\n";
+    return  nameString + stateString + needsString + givesString + "\n";
   }
 }
