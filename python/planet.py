cb0e5ea83b7ce01d762beb7669bd86297672bf32
nate_chapman <chapmann19@gmail.com>
2023-09-03 13:10:44 -0700

    change needs and gives to sets

--- python/planet.py
+++ python/planet.py
@@ -2,11 +2,11 @@
 
 import random as rng
 
-resources_list  = ["food", "water", "air", "materials", "people", "entertainment"];
+resources_list  = ["food", "water", "air", "materials", "people", "entertainment"]
 needs_gives_state = "NG"
 needs_state = "N_"
 gives_state = "_G"
-satisfied_state = "__";
+satisfied_state = "__"
 
 class Planet:
     # TODO: add static planet count and use it as the id
@@ -14,8 +14,8 @@ class Planet:
         self.name = hex(rng.randint(16**6, 16**7 - 1))[2:].upper() # make the name a 6 character hex string
         self.id   = "Come back to this"
         self.state = needs_gives_state
-        self.needs = rng.sample(resources_list, 2)
-        self.gives = rng.sample(resources_list, 2)
+        self.needs = {*rng.sample(resources_list, 2)}
+        self.gives = {*rng.sample(resources_list, 2)}
 
     def summary(self):
         print(f"name: {self.name}")
