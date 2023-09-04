ea996e24de10430571c2effbb81d043357cb408e
nate_chapman <chapmann19@gmail.com>
2023-09-03 23:03:58 -0700

    change network matrix to zeros and ones

--- python/network.py
+++ python/network.py
@@ -4,11 +4,11 @@
 from planet import Planet, update_needs_gives
 
 def extend_network(network):
-    new_row = ["" for i in range(len(network))]
+    new_row = [0 for i in range(len(network))]
     network.append(new_row) # append a row of the right length
     
     for row in network:
-        row.append("")
+        row.append(0)
 
 def add_planet(planet_list, network, planet : Planet):
     planet.id = len(planet_list)
@@ -16,8 +16,8 @@ def add_planet(planet_list, network, planet : Planet):
     extend_network(network)
 
 def connect(network, planet_1 : Planet, planet_2 : Planet):
-    network[planet_1.id][planet_2.id] = [planet_1.state, planet_2.state]
-    network[planet_2.id][planet_1.id] = [planet_2.state, planet_1.state]
+    network[planet_1.id][planet_2.id] = 1
+    network[planet_2.id][planet_1.id] = 1
 
 def trade(network, planet_1 : Planet, planet_2 : Planet):
     if planet_1 is not planet_2:
